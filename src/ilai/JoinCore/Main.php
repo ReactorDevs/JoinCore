26e34ffe7f5fb58f1463d5a9faa10a7a02502da0
ilaicraftYT <70978042+ilaicraftYT@users.noreply.github.com>
2021-08-08 11:50:37 -0500

    added base code

--- src/ilai/JoinCore/Main.php
+++ src/ilai/JoinCore/Main.php
@@ -1,1 +1,12 @@
+<?php
 
+declare(strict_types=1);
+
+namespace ethaniccc\NoDebuffBot;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+}
